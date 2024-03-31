ce40ee311fbf86592d8528326273a02b68a421aa
Justin Odel <JustInJax1991-SvcAcct@outlook.com>
2024-03-31 08:36:06 -0400

    addition of variables.tf with variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "bucket_name" {
+    type = string
+    description = "Name of the hosting bucket"
+}
+variable "region" {
+    type = string
+    description = "us-east-1"
+}
+variable "access_key" {
+    type = string
+}
+variable "secret_key" {
+    type = string
+}
